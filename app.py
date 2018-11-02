7f76399bbbebbb76442dc77111ae571d956c080e
Connor Barrett <connor.barrett@wisc.edu>
2018-11-02 11:52:47 -0500

    added upload and download buttons

--- app.py
+++ app.py
@@ -1,12 +1,13 @@
 from flask import Flask, render_template
-
+from flask_wtf.file import FileField
+import pandas as pd
 app = Flask(__name__)
 
 
 @app.route('/')
 def index():
 	return render_template('index.html')
-
+	# df = pd.read_excel()
 
 if __name__ == '__main__':
 	app.run(debug=False)
